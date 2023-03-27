7f1aaff08a5ce2861ff28cee2bebb86fa0276abb
Abdur Rahim <arfat01850@gmail.com>
2023-03-27 11:33:08 +0600

    Add files via upload

---
+++ Module6Practice/SlidePractice.php
@@ -0,0 +1,24 @@
+<?php
+
+// $datezone = date("d.m.Y")."\n";
+// $weekName = date("l")."\n";
+
+// echo $datezone;
+// echo $weekName;
+
+// echo date("Y/m/d")."\n";
+// echo date("Y.m.d")."\n";
+// echo date("Y-m-d")."\n";
+// echo date("Y-m-d")."\n";
+
+// $time = date("h:i:sa");
+// echo $time;
+
+
+// $anotherTimezone = date_default_timezone_set('Asia/Dhaka');
+// $time = date("h:i:sa");
+// echo $time;
+
+
+//date add()
+
