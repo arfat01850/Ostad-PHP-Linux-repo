1f4b9f9fa1b463f2b04be48a050660432698619b
arfat01850 <arfat01850@gmail.com>
2023-03-29 20:01:05 +0600

    mod8 file add

--- Module8Practice/OOP-HasinVai/OOP3.php
+++ Module8Practice/OOP-HasinVai/OOP3.php
@@ -1,1 +1,23 @@
 <?php
+
+class MathHelper {
+    public static function square($n){
+        return $n*$n;
+    }
+    public static function cube($n){
+        return $n*$n*$n;
+    }
+}
+
+$squareRoot =  MathHelper::square(4);
+$cubeRoot =  MathHelper::cube(4);
+echo $squareRoot;
+echo "\n";
+echo $cubeRoot;
+
+// $mh = new MathHelper();
+// $SquareRootof4 = $mh->square(4);
+// $cubeRootof4 = $mh->cube(4);
+// echo $SquareRootof4;
+// echo "\n";
+// echo $cubeRootof4;
