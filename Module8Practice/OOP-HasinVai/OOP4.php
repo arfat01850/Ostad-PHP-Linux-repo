0710e79fc60e6c77294a38061fc9806e0e0e9f9b
arfat01850 <arfat01850@gmail.com>
2023-05-04 15:45:35 +0600

    Update file

---
+++ Module8Practice/OOP-HasinVai/OOP4.php
@@ -0,0 +1,23 @@
+<?php
+
+interface Shape{
+    function getArea();
+}
+class Rectangle implements Shape{
+    function getArea(){
+
+    }
+}
+
+class Circle implements Shape{
+    function getArea(){
+        
+    }
+}
+
+class Rombush implements Shape{
+    function getArea(){
+        
+    }
+}
+
